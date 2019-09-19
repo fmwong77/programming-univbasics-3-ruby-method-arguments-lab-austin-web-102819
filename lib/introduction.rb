cf1a1a194e49ed449c1cbc4dbe109e154b530f12
Mei Wong <fongmeiw@gmail.com>
2019-09-19 05:09:18 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -6,7 +6,7 @@
 def introduction(name)
   puts "Hi, my name is #{name}"
 end
-
+=begin
 def introduction_with_language(name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}"
 end
@@ -14,8 +14,8 @@ end
 def introduction_with_language_optional(name, language="Ruby")
   puts "Hi, my name is #{name} and I am learning to program in #{language}"
 end
-
-
+=end
+introduction("Mei")
 
 
 
